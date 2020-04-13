4a8d61d8884c24b2c721f4b3e6714dfc00821691
javierdelpozolopez <javierdelpozolopez@gmail.com>
2020-04-13 16:00:09 -0400

    title portafolio added

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -7,6 +7,8 @@ import Fluid from '../components/home/our-work/fluid';
 const Portfolio = () => (
   <Layout>
     <Fluid />
+    <h1>Somos ninjas en la oscuridad</h1>
+    <p>Trabajamos para agencias que necesitan ayuda en desarrollar sus ideas digitales. Es por eso que nuestros proyectos tienen algunos niveles de privacidad que no nos permite publicarlos directamente en la web, si deseas ver todo nuetro trabajo, puedes pedirnos acceso a nuestro portafolio online aquí.</p>
   </Layout>
 );
 
