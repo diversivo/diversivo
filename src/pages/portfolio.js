11c5460f38468600788ef8120bb6f1ee9b59f989
javierdelpozolopez <javierdelpozolopez@gmail.com>
2020-05-10 18:37:23 -0400

    portfolio container added

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import Layout from '../components/layout';
+import SEO from '../components/seo';
+
 // import { Link } from 'gatsby';
 
-import Fluid from '../components/home/our-work/fluid';
+// import Fluid from '../components/home/our-work/fluid';
 
 const Portfolio = () => (
   <Layout>
-    <Fluid />
-    <h1>Somos ninjas en la oscuridad</h1>
-    <p>Trabajamos para agencias que necesitan ayuda en desarrollar sus ideas digitales.</p>
-    <p>Es por eso que nuestros proyectos tienen algunos niveles de privacidad que no nos permite publicarlos directamente en la web, si deseas ver todo nuetro trabajo, puedes pedirnos acceso a nuestro portafolio online aquí.</p>
-    </Layout>
+    <SEO title="Diversivo - Trabajos" />
+    <div className="container">
+      <div className="content">
+        <h1>Somos ninjas en la oscuridad</h1>
+        <p>Trabajamos para agencias que necesitan ayuda en desarrollar sus ideas digitales.</p>
+        <p>Es por eso que nuestros proyectos tienen algunos niveles de privacidad que no nos permite publicarlos directamente en la web, si deseas ver todo nuetro trabajo, puedes pedirnos acceso a nuestro portafolio online.</p>
+      </div>
+    </div>
+  </Layout>
 );
 
 export default Portfolio;
