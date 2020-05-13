50d2a5df778502b785be2bdfed88118402d75ae2
javierdelpozolopez <javierdelpozolopez@gmail.com>
2020-05-13 12:10:20 -0400

    our-work css edit for better view

--- src/components/home/our-work/index.js
+++ src/components/home/our-work/index.js
@@ -4,59 +4,31 @@ import { Link } from 'gatsby';
 import Fluid from './fluid';
 
 const OurWork = () => (
-  <div style={{
-    position: 'relative',
-  }}
-  >
-    <div
-      className="container"
-      style={{
-        pointerEvents: 'none',
-        color: 'black',
-        zIndex: 1000,
-        position: 'absolute',
-      }}
-    >
-      <div
-        className="content"
-        style={{
-          padding: '10rem 0 6rem 0',
+  <div className="container-fluid">
+    <div className="fluid__content flex-cc">
+      <div className="text-align-center">
+        <pre style={{
+          color: '#A8A8A8',
         }}
-      >
-        <div
-          className="flex-cc"
-          style={{
-            textAlign: 'center',
-          }}
         >
-          <pre style={{
-            color: '#A8A8A8',
-          }}
-          >
-            TE PODEMOS AYUDAR EN
-          </pre>
-          <h1 className="bigger-title">
-            Desarrollo de Software,
-            <br />
-            Branding y Marketing Digital.
-          </h1>
-          <Link
-            className="content__link"
-            to="/our-work/"
-          >
-            QUE HACEMOS
-          </Link>
-        </div>
+          TE PODEMOS AYUDAR EN
+        </pre>
+        <h1 className="bigger-title">
+          Desarrollo de Software,
+          <br />
+          Branding y Marketing Digital.
+        </h1>
+      </div>
+      <div className="fluid__content-link">
+        <Link
+          className="fluid__link"
+          to="/our-work/"
+        >
+          QUE HACEMOS
+        </Link>
       </div>
     </div>
-    <Fluid style={{
-      color: 'white',
-      height: '100%', // Canvas is will respond to size changes without resetting fluid!
-      width: '100%',
-      margin: 0,
-      position: 'relative',
-    }}
-    />
+    <Fluid />
   </div>
 );
 
