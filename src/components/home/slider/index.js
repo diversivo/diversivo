4d6ede38c3717f120eefd9479d869e721754480f
javierdelpozolopez <javierdelpozolopez@gmail.com>
2020-05-12 22:47:34 -0400

    main art update

--- src/components/home/slider/index.js
+++ src/components/home/slider/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import bgArt from '../../../assets/video/main-bg-art.mp4';
-// import bgArtPortrait from '../../../assets/video/main-bg-art-portrait.mp4';
 
 const Slider = () => (
   <div className="main-slider">
     <video className="main-slider__bg" playsInline autoPlay muted loop id="video">
       <source src={bgArt} type="video/mp4" />
-      {/* <source src={bgArtPortrait} type="video/mp4" /> */}
     </video>
     <div className="main-slider__text">
       <pre> bienvenido </pre>
